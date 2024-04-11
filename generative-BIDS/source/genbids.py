ea46c8e3f68e115b1dd57c0c09fd0756e0a388bc
Marcel Zwiers <m.zwiers@donders.ru.nl>
2024-04-11 23:37:35 +0200

    Create initial pseudo-randomization code

--- generative-BIDS/source/genbids.py
+++ generative-BIDS/source/genbids.py
@@ -6,13 +6,44 @@
 
 import argparse
 import textwrap
+import pandas as pd
+import shutil
 from pathlib import Path
 
-def main(inputdir: str, outputdir: str, include: list[str], properties: dict):
+def main(inputdir: str, outputdir: str, properties: dict, include: list[str]):
 
     # Defaults
     inputdir  = Path(inputdir).resolve()
     outputdir = Path(outputdir).resolve()
+
+    for pattern in include:
+
+        inputfiles = inputdir.rglob(pattern)
+
+        for inputfile in inputfiles:
+
+            inputdata  = pd.DataFrame()
+            outputfile = outputdir/inputfile.relative_to(inputdir)
+
+            # Load the data, pseudo-randomise it and save it
+            if inputfile.suffix == '.tsv':
+                inputdata = pd.read_csv(inputfile, sep='\t', index_col='filename')
+            elif inputfile.suffix == '.json':
+                shutil.copyfile(inputfile, outputfile)  # WIP
+            else:
+                shutil.copyfile(inputfile, outputfile)
+
+            # Permute the participant label
+            inputdata.sample(frac=1)
+
+            # Permute columns that are not of interest
+            for column in inputdata.columns:
+                if column not in properties:
+                    pass    # WIP
+
+            # Save the output data
+            if inputdata:
+                inputdata.to_csv(outputfile, sep='\t', encoding='utf-8', na_rep='n/a')
 
 
 # Shell usage
@@ -33,4 +64,4 @@ if __name__ == "__main__":
     parser.add_argument('include',    help='The include pattern(s) that select the files in the BIDS input-directory that need to be generated in the output directory', nargs='+', default='*')
     args = parser.parse_args()
 
-    main(inputdir=args.inputdir, outputdir=args.outputdir, include=args.include, properties=args.properties)
+    main(inputdir=args.inputdir, outputdir=args.outputdir, properties=args.properties, include=args.include)
