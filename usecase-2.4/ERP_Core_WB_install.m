26f881267eb58e3a699b30f40b0d0e0e26fec43e
CPernet <wamcyril@gmail.com>
2024-05-22 09:10:35 +0200

    check all plugins even defaults ones

--- usecase-2.4/ERP_Core_WB_install.m
+++ usecase-2.4/ERP_Core_WB_install.m
@@ -1,11 +1,69 @@
+% add EEGLAB -- edit path as needed
 addpath('eeglab2024.0');
-eeglab; % otherwise it won't find plugin_askinstall
+
+% the rest runs automatically
+eeglab('nogui')
+pluginfolder = fullfile(fileparts(which('eeglab.m')),'plugins');
 
 plugin_askinstall('bids-matlab-tools',[],1);
+if ~exist('pop_importbids','file')
+    try 
+        folder2add = dir(fullfile(pluginfolder,"bids-matlab-tools*"));
+        addpath(fullfile(pluginfolder,folder2add.name)); 
+    end
+end
 plugin_askinstall('zapline-plus',[],1);
-plugin_askinstall('picard',[],1);
+if ~exist('pop_zapline_plus','file')
+    try 
+        folder2add = dir(fullfile(pluginfolder,"zapline-plus*"));
+        addpath(fullfile(pluginfolder,folder2add.name)); 
+    end
+end
+plugin_askinstall('clean_rawdata',[],1);
+if ~exist('pop_clean_rawdata','file')
+    try 
+        folder2add = dir(fullfile(pluginfolder,"clean_rawdata*"));
+        addpath(fullfile(pluginfolder,folder2add.name)); 
+    end
+end
+plugin_askinstall('picard', 'picard', 1);
+if ~exist('picard','file')
+    try 
+        folder2add = dir(fullfile(pluginfolder,"PICARD*"));
+        addpath(fullfile(pluginfolder,folder2add.name)); 
+    end
+end
+plugin_askinstall('ICLabel', 'ICLabel', 1);
+if ~exist('pop_iclabel','file')
+    try 
+        folder2add = dir(fullfile(pluginfolder,"ICLabel*"));
+        addpath(fullfile(pluginfolder,folder2add.name)); 
+    end
+end
+plugin_askinstall('Fieldtrip-lite', 'Fieldtrip-lite', 1);
+if ~exist('ft_prepare_neighbours','file')
+    try 
+        folder2add = dir(fullfile(pluginfolder,"Fieldtrip-lite*"));
+        addpath(fullfile(pluginfolder,folder2add.name)); 
+    end
+end
+if ~exist('limo_eeg','file')
+    try 
+        folder2add = dir(fullfile(pluginfolder,"limo_tools*"));
+        addpath(fullfile(pluginfolder,folder2add.name)); 
+    end
+end
 
-addpath(fullfile(pwd, 'eeglab2024.0/plugins/zapline-plus1.2.1/zapline-plus-1.2.1');
-addpath(fullfile(pwd, 'eeglab2024.0/plugins/bids-matlab-tools8.0/bids-matlab-tools');
-addpath(fullfile(pwd, 'eeglab2024.0/plugins/Fieldtrip-lite20240111/fieldtrip-20240111');
-addpath(fullfile(pwd, 'eeglab2024.0/plugins/PICARD1.0/picard-matlab');
+% if still not there retrun an error message
+if ~exist('pop_importbids','file') || ...
+        ~exist('pop_zapline_plus','file') || ...
+        ~exist('pop_clean_rawdata','file') || ...
+        ~exist('picard','file') || ...
+        ~exist('pop_iclabel','file') || ...
+        ~exist('ft_prepare_neighbours','file') || ...
+        ~exist('limo_eeg','file')
+    error('1 or more of the necessary plugins is not found');
+else
+    disp('all plugins found')
+    savepath
+end
