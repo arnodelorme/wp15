8794aa71678e77692c866d5f66d4bf3002e3e74c
CPernet <wamcyril@gmail.com>
2024-05-22 09:16:25 +0200

    use empty in plugin call

--- usecase-2.4/ERP_Core_WB_install.m
+++ usecase-2.4/ERP_Core_WB_install.m
@@ -26,21 +26,21 @@ if ~exist('pop_clean_rawdata','file')
         addpath(fullfile(pluginfolder,folder2add.name)); 
     end
 end
-plugin_askinstall('picard', 'picard', 1);
+plugin_askinstall('picard', [], 1);
 if ~exist('picard','file')
     try 
         folder2add = dir(fullfile(pluginfolder,"PICARD*"));
         addpath(fullfile(pluginfolder,folder2add.name)); 
     end
 end
-plugin_askinstall('ICLabel', 'ICLabel', 1);
+plugin_askinstall('ICLabel', [], 1);
 if ~exist('pop_iclabel','file')
     try 
         folder2add = dir(fullfile(pluginfolder,"ICLabel*"));
         addpath(fullfile(pluginfolder,folder2add.name)); 
     end
 end
-plugin_askinstall('Fieldtrip-lite', 'Fieldtrip-lite', 1);
+plugin_askinstall('Fieldtrip-lite', [], 1);
 if ~exist('ft_prepare_neighbours','file')
     try 
         folder2add = dir(fullfile(pluginfolder,"Fieldtrip-lite*"));
