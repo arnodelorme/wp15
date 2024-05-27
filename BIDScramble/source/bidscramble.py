f0572688f4e09f7edf1faa6d461ad281f5f34d2c
Marcel Zwiers <m.zwiers@donders.ru.nl>
2024-05-27 16:55:35 +0200

    Test pwd

--- BIDScramble/source/bidscramble.py
+++ BIDScramble/source/bidscramble.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-
+Creates a copy of the input directory in which all files are empty. Exceptions to this are the
+'dataset_description.json', the 'README' and the 'LICENSE' files, which are copied over and updated
+if they exist.
 """
 
 import argparse
 import textwrap
-import pandas as pd
 import shutil
-import numpy as np
 import json
 from urllib.request import urlopen
 from pathlib import Path
@@ -21,6 +21,13 @@ def bidscramble(inputdir: str, outputdir: str, covariance: list[str], include: l
     inputdir  = Path(inputdir).resolve()
     outputdir = Path(outputdir).resolve()
     outputdir.mkdir(parents=True, exist_ok=True)
+
+    # Create placeholder output files for all input files
+    for inputfile in inputdir.rglob('*'):
+        if inputfile.is_dir():
+            (outputdir/inputfile.relative_to(inputdir)).mkdir(parents=True, exist_ok=True)
+        else:
+            (outputdir/inputfile.relative_to(inputdir)).touch()
 
     # Create a dataset description file
     dataset_file = inputdir/'dataset_description.json'
@@ -54,39 +61,6 @@ def bidscramble(inputdir: str, outputdir: str, covariance: list[str], include: l
                 break
         license_file.write_text(license)
 
-    # Create pseudo-random out data for all files of each included data type
-    for pattern in include:
-
-        inputfiles = inputdir.rglob(pattern)
-
-        for inputfile in inputfiles:
-
-            # Define the output target
-            outputfile = outputdir/inputfile.relative_to(inputdir)
-
-            # Load or copy the data
-            inputdata = pd.DataFrame()
-            if inputfile.suffix == '.tsv':
-                print(f"Reading: {inputfile}")
-                inputdata = pd.read_csv(inputfile, sep='\t')
-            elif inputfile.suffix == '.json':
-                print(f"Saving: {outputfile}")
-                shutil.copyfile(inputfile, outputfile)  # WIP
-                continue
-            else:
-                print(f"Saving: {outputfile}")
-                shutil.copyfile(inputfile, outputfile)
-                continue
-
-            # Permute columns that are not of interest
-            for column in inputdata.columns:
-                if column not in covariance:
-                    inputdata[column] = np.random.permutation(inputdata[column])
-
-            # Save the output data
-            print(f"Saving: {outputfile}")
-            inputdata.to_csv(outputfile, sep='\t', index=False, encoding='utf-8', na_rep='n/a')
-
 
 def main():
     """Console script entry point"""
@@ -97,16 +71,14 @@ def main():
 
     parser = argparse.ArgumentParser(formatter_class=CustomFormatter, description=textwrap.dedent(__doc__),
                                      epilog='examples:\n'
-                                            '  bidscramble bids pseudobids -c age sex height -i *.tsv *.json CHANGES README\n\n'
+                                            '  bidscramble bids pseudobids\n\n'
                                             'author:\n'
                                             '  Marcel Zwiers\n ')
-    parser.add_argument('inputdir',           help='The BIDS input-directory with the real data')
-    parser.add_argument('outputdir',          help='The BIDS output-directory with generated pseudo data')
-    parser.add_argument('-c', '--covariance', help='A list of variable names between which the covariance structure is preserved when generating the pseudo data', nargs='+')
-    parser.add_argument('-i', '--include',    help='A list of include pattern(s) that select the files in the BIDS input-directory that are produced in the output directory', nargs='+', default=['*'])
+    parser.add_argument('inputdir',  help='The BIDS input-directory with the real data')
+    parser.add_argument('outputdir', help='The BIDS output-directory with empty pseudo data')
     args = parser.parse_args()
 
-    bidscramble(inputdir=args.inputdir, outputdir=args.outputdir, covariance=args.covariance, include=args.include)
+    bidscramble(inputdir=args.inputdir, outputdir=args.outputdir)
 
 
 if __name__ == "__main__":
---
+++ BIDScramble/source/bidscramble_tsv.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Adds scrambled versions of the tsv files in the input directory to the output directory.
+"""
+
+import argparse
+import textwrap
+import pandas as pd
+import shutil
+import numpy as np
+from pathlib import Path
+
+
+def bidscramble(inputdir: str, outputdir: str, covariance: list[str], include: list[str]):
+
+    # Defaults
+    inputdir  = Path(inputdir).resolve()
+    outputdir = Path(outputdir).resolve()
+    outputdir.mkdir(parents=True, exist_ok=True)
+
+    # Create pseudo-random out data for all files of each included data type
+    for pattern in include:
+
+        inputfiles = inputdir.rglob(pattern)
+
+        for inputfile in inputfiles:
+
+            # Define the output target
+            outputfile = outputdir/inputfile.relative_to(inputdir)
+
+            # Load or copy the data
+            inputdata = pd.DataFrame()
+            if inputfile.suffix == '.tsv':
+                print(f"Reading: {inputfile}")
+                inputdata = pd.read_csv(inputfile, sep='\t')
+            else:
+                print(f"Saving: {outputfile}")
+                shutil.copyfile(inputfile, outputfile)
+                continue
+
+            # Permute columns that are not of interest
+            for column in inputdata.columns:
+                if column not in covariance:
+                    inputdata[column] = np.random.permutation(inputdata[column])
+
+            # Save the output data
+            print(f"Saving: {outputfile}")
+            inputdata.to_csv(outputfile, sep='\t', index=False, encoding='utf-8', na_rep='n/a')
+
+
+def main():
+    """Console script entry point"""
+
+    # Parse the input arguments and run main(args)
+    class CustomFormatter(argparse.ArgumentDefaultsHelpFormatter, argparse.RawDescriptionHelpFormatter):
+        pass
+
+    parser = argparse.ArgumentParser(formatter_class=CustomFormatter, description=textwrap.dedent(__doc__),
+                                     epilog='examples:\n'
+                                            '  bidscramble bids pseudobids -c age sex height -i *.tsv *.json CHANGES README\n\n'
+                                            'author:\n'
+                                            '  Marcel Zwiers\n ')
+    parser.add_argument('inputdir',           help='The BIDS input-directory with the real data')
+    parser.add_argument('outputdir',          help='The BIDS output-directory with generated pseudo data')
+    parser.add_argument('-c', '--covariance', help='A list of variable names between which the covariance structure is preserved when generating the pseudo data', nargs='+')
+    parser.add_argument('-i', '--include',    help='A list of include pattern(s) that select the files in the BIDS input-directory that are produced in the output directory', nargs='+', default=['*'])
+    args = parser.parse_args()
+
+    bidscramble(inputdir=args.inputdir, outputdir=args.outputdir, covariance=args.covariance, include=args.include)
+
+
+if __name__ == "__main__":
+    main()
