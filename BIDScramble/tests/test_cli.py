232f6a2a2bc797df9c4f340d4f0c74b59ffa1f07
Marcel Zwiers <m.zwiers@donders.ru.nl>
2024-05-28 17:00:57 +0200

    Add cli test

---
+++ BIDScramble/tests/test_cli.py
@@ -0,0 +1,15 @@
+import subprocess
+from importlib.metadata import entry_points
+
+
+def test_cli():
+
+    console_scripts = entry_points().select(group='console_scripts')    # The select method was introduced in python = 3.10
+
+    entrypoints = []
+    for script in console_scripts:
+        if script.value.startswith('source'):
+            entrypoints.append(script.name)
+            process = subprocess.run(f"{script.name} -h", shell=True)
+            assert process.returncode == 0
+    assert 'bidscramble' in entrypoints
