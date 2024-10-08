7c16e7d56c4fe566cb6687af9e7f89f8a955dfe3
Marque-CerCo <adrien.romain.marque@gmail.com>
2024-10-08 13:36:44 +0200

    Update unzipDatabase.py

--- usecase-2.5/source/unzipDatabase.py
+++ usecase-2.5/source/unzipDatabase.py
@@ -3,15 +3,18 @@ import sys
 import gzip
 
 current_directory = os.getcwd();
-list_strings = current_directory.split('/');path_home = '/' + list_strings[1];
+list_strings = current_directory.split('/');
 path_home = '/' + list_strings[1];
 path_username = os.path.join(path_home, list_strings[2]);
-path_input = os.path.join(path_username, 'input');
+path_input = os.path.join(path_username, 'ds003020');
+print(path_home);
+print(path_username);
+print(path_input);
 list_subjects = os.listdir(path_input);
 
 for sub in list_subjects:
     if sub[:3] == "sub":
-        path_subject = os.path.join(current_directory, sub);
+        path_subject = os.path.join(path_input, sub);
         list_sessions = os.listdir(path_subject);
         for ses in list_sessions:
             print(list_sessions);
@@ -24,14 +27,14 @@ for sub in list_subjects:
                     path_fmri = os.path.join(path_session, d);
                     list_files = os.listdir(path_fmri);
                     for f in list_files:
-                        path_file = os.path.join(path_fmri, f)                       
+                        path_file = os.path.join(path_fmri, f) 
                         if f[-3:] == '.gz':
                             check_symlink = os.path.islink(path_file);
                             if check_symlink == True:
                                 path_symlink = os.readlink(path_file);
                                 list_words = path_symlink.split("/");
                                 filename = list_words[-1];
-                                real_symlink = current_directory;
+                                real_symlink = path_input;
 
                                 for w in range(3, len(list_words)):
                                     real_symlink = os.path.join(real_symlink, list_words[w]);
@@ -53,4 +56,3 @@ for sub in list_subjects:
                                 command_gzip = 'gzip -d ' + path_file;
                                 os.system(command_gzip);                        
                                 
-            
