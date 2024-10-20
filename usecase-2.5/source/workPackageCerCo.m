526b23e75eecc8530c23b90ea35b4c255760f91b
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2024-10-20 12:17:35 +0200

    ENH - some changes to facilitate multiple tests (on subsets of subjects), removed unzipping part (which will be done somewhere else in the code)

--- usecase-2.5/source/workPackageCerCo.m
+++ usecase-2.5/source/workPackageCerCo.m
@@ -1,3 +1,9 @@
+function workPackageCerCo(run_list)
+
+if nargin<1
+  run_list = {};
+end
+
 time_start = tic;
 
 m = mfilename('fullpath');
@@ -11,9 +17,9 @@ path_username = fullfile(filesep,s{1:sel});
 
 % % build some paths
 path_spm12 = fullfile(path_username, 'spm12');
-path_fieldtrip = fullfile(path_spm12, 'external', 'fieldtrip');
-path_fileio = fullfile(path_fieldtrip, 'fileio');
-path_freesurfer = fullfile(path_fieldtrip, 'external/freesurfer');
+%path_fieldtrip = fullfile(path_spm12, 'external', 'fieldtrip');
+%path_fileio = fullfile(path_fieldtrip, 'fileio');
+%path_freesurfer = fullfile(path_fieldtrip, 'external/freesurfer');
 path_config = fullfile(path_spm12, 'config');
 path_matlabbatch = fullfile(path_spm12, 'matlabbatch');
 path_input = fullfile(path_username, 'input');
@@ -22,23 +28,12 @@ path_workpackage = fullfile(path_username, s{(sel+1):end});
 
 % addpath, for matlab and use them 
 addpath(path_spm12);
-addpath(path_fieldtrip);
-addpath(path_fileio);
-addpath(path_freesurfer);
+% addpath(path_fieldtrip);
+% addpath(path_fileio);
+% addpath(path_freesurfer);
 addpath(path_config);
 addpath(path_matlabbatch);
 addpath(path_workpackage);
-
-% unzip nifti files
-cd(path_input)
-subs = dir('sub-*/*/*.gz');
-for k = 1:numel(subs)
-    path_file = fullfile(subs(k).folder, subs(k).name);
-    disp(path_file);
-    gunzip(fullfile(subs(k).folder, subs(k).name));
-    delete(fullfile(subs(k).folder, subs(k).name));
-end
-
 
 % initialize spm12 via matlab
 spm('defaults','fmri');
@@ -47,7 +42,9 @@ spm_get_defaults('cmdline',true);
 
 % remove the subject 006
 pathSubject_delete = fullfile(path_input, 'sub-SAXNES2s006');
-removeSubject(pathSubject_delete);
+if exist(pathSubject_delete, 'dir')
+  removeSubject(pathSubject_delete);
+end
 
 % convert tsv files to txt files located at path_input 
 convert_tsv_to_txt(path_input);
@@ -55,8 +52,8 @@ convert_tsv_to_txt(path_input);
 % create output architecture directory based on input directory
 createDataStructure(path_input, path_output);
 
-% apply a list of transformations to niftii files (anat + func)
-patientsDatabase(path_username, path_input, path_output);
+% apply a list of transformations to nifti files (anat + func)
+patientsDatabase(path_input, path_output, run_list);
 
 % clean useless files
 cleanDatabase(path_input);
