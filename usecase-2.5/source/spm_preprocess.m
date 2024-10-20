a75f4d4cbe7c5722e1d7d6f54f906d45d13398a5
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2024-10-20 12:15:47 +0200

    ENH - do the unzipping (if needed) closer to the actual computations (so that not the whole database needs to be unzipped if testing is done only on a lower amount of subjects), and improve the logic of dealing with the files (i.e. select only those starting with 'sub' which takes away the need to iterate in the for loop from number 3)

--- usecase-2.5/source/spm_preprocess.m
+++ usecase-2.5/source/spm_preprocess.m
@@ -1,44 +1,50 @@
 function structPreProcess = spm_preprocess(path_input)
-	
-	path_source = '';
-	path_anat = fullfile(path_input, 'anat');
-	list_files = dir(path_anat);
-	szFiles = size(list_files);
 
-	for f = 3:szFiles(1)
-		path_file = fullfile(path_anat, list_files(f).name);
-		check_nifti = endsWith(list_files(f).name, '.nii');
-		
-		if check_nifti == true
-			path_source = path_file;
-		end
-	end
+path_source = '';
+path_anat = fullfile(path_input, 'anat');
+list_files = dir(fullfile(path_anat, 'sub*nii.gz'));
+if ~isempty(list_files)
+  for f = 1:numel(list_files)
+    fname = fullfile(list_files(f).folder, list_files(f).name);
+    fprintf('unzipping %s', fname)
+    gunzip(fname);
+    delete(fname);
+  end
+end
+list_files = dir(fullfile(path_anat, 'sub*nii'));
+szFiles = size(list_files);
 
-	list_fmris = dir(path_input);
-	szFMRIs = size(list_fmris);	
+for f = 1:szFiles(1)
+  path_file   = fullfile(path_anat, list_files(f).name);
+  path_source = path_file;
+end
 
-	for m = 3:szFMRIs(1)
-		path_fmri = fullfile(path_input, list_fmris(m).name);
-		check_fmri = isfolder(path_fmri);			
-		structPreProcess = struct;
+path_func = fullfile(path_input, 'func'); % according to BIDS, functional MRIs are always stored in func folders
+list_fmris = dir(fullfile(path_func, 'sub*nii.gz')); 
+if ~isempty(list_fmris)
+  for f = 1:numel(list_fmris)
+    fname = fullfile(list_fmris(f).folder, list_fmris(f).name);
+    fprintf('unzipping %s', fname)
+    gunzip(fname);
+    delete(fname);
+  end
+end
+list_fmris = dir(fullfile(path_func, 'sub*nii'));
+szFMRIs = size(list_fmris);
 
-		if check_fmri == true		
-			disp('Step 2 -- Realign all volumes to first functional volume');
-			realign(path_fmri);
-			disp('Step 2 - Done!');
-		end 
-								
-		list_files = dir(path_fmri);
-		szFiles = size(list_files);
-				
-		path_func = fullfile(path_input, 'func');
-		
-		disp('Step 3 -- Coregister structural image to first dynamic image');
-		coreg(path_source, path_func);
-		disp('Step 3 - Done!'); 
-									
-		disp('Step 4 -- Gaussian kernel smoothing of realigned data');
-		smooth(path_anat);
-		disp('Step 4 is done !');
-     end
+for m = 1:szFMRIs(1)
+  structPreProcess = struct;
+
+  disp('Step 2 -- Realign all volumes to first functional volume');
+  realign(path_func);
+  disp('Step 2 - Done!');
+  
+  disp('Step 3 -- Coregister structural image to first dynamic image');
+  coreg(path_source, path_func);
+  disp('Step 3 - Done!');
+
+  disp('Step 4 -- Gaussian kernel smoothing of realigned data');
+  smooth(path_func); % FIXME CHECK WITH CerCo PEOPLE, the input to smooth() was path_anat at first, is that correct????
+  disp('Step 4 is done !');
+end
 end
