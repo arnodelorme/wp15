2d207661d940783b1e3c44f26601d3a717fbede3
Jan Mathijs Schoffelen <j.schoffelen@donders.ru.nl>
2024-10-20 12:13:32 +0200

    FIX - the contrasts were computed for all subject, should be for one patient only, if I understand the code logic well enough

--- usecase-2.5/source/onePatient.m
+++ usecase-2.5/source/onePatient.m
@@ -1,45 +1,32 @@
-% apply a list of transformations on niftii files and written results to the correct output directory
-function onePatient(path_username, path_subject, path_output)
+% apply a list of transformations on nifti files and write results to the correct output directory
+function onePatient(path_subject, path_output)
 
-	% apply list of transformations to fMRIs
-    	spm_preprocess(path_subject);
-    	disp('preProcess is done !');
-		
-	path_input = fullfile(path_username, 'input');
-	list_subjects = dir(path_input);
-	szSubjects = size(list_subjects);
+% apply list of transformations to fMRIs, preceded by unzipping if needed: realignment, coregistration, smoothing
+spm_preprocess(path_subject);
+disp('preProcess is done !');
 
-	contrast_names = "contrast";
-    	convec = [1 -1];
+contrast_names = 'contrast';
+convec = [1 -1];
 
-	for sub = 3:szSubjects(1)
-		path_subject = fullfile(path_input, list_subjects(sub).name);
-		path_func = fullfile(path_subject, 'func');
-		list_files = dir(path_func);
-		szFiles = size(list_files);
-		for f = 3:szFiles(1)
-			path_file = fullfile(path_func, list_files(f).name);
-			check_sub = startsWith(list_files(f).name, 'sub');
-			check_nifti = endsWith(list_files(f).name, '.nii');
-			levels = {};
-			if check_sub == true && check_nifti == true
-				path_txt = fullfile(path_func, list_files(f+2).name);
-				events = dataEvents(path_txt, 'event');
-				onset =  dataEvents(path_txt, 'onset');
-				duration =  dataEvents(path_txt, 'duration');
+path_func = fullfile(path_subject, 'func');
+list_files = dir(fullfile(path_func, 'sub*nii'));
+szFiles = size(list_files);
+for f = 1:szFiles(1)
+  path_txt = fullfile(path_func, list_files(f+2).name); % FIXME this is dangerous, because it assumes a fixed order (and number) of files
+  events = dataEvents(path_txt, 'event'); %FIXME consider adjusting dataEvents so that it can operate on the *tsv files directly, which takes away the need to convert the tsv files to txt first
+  onset =  dataEvents(path_txt, 'onset');
+  duration =  dataEvents(path_txt, 'duration');
 
-				levels = create_levelParameters(2, 'intact', events, onset, duration);
-				disp('specify first level is done !');
+  levels = create_levelParameters(2, 'intact', events, onset, duration);
+  disp('specify first level is done !');
 
-				firstLevel(path_subject, path_output, levels);
-				disp('firstLevel is done !');
+  firstLevel(path_subject, path_output, levels);
+  disp('firstLevel is done !');
 
-				estimateModel(path_output, false);
-				disp('estimateModel is done !');
+  estimateModel(path_output, false);
+  disp('estimateModel is done !');
 
-				contrasts(path_output, contrast_names, convec);
-				disp('contrasts is done !');
-			end
-		end
-	end 
+  contrasts(path_output, contrast_names, convec);
+  disp('contrasts is done !');
+end
 end
