46724d45d89ba4069c41cc5688b6bad4cfe18250
Marque-CerCo <adrien.romain.marque@gmail.com>
2024-10-30 11:07:09 +0100

    Update workPackageCerCo.m

--- usecase-2.5/source/workPackageCerCo.m
+++ usecase-2.5/source/workPackageCerCo.m
@@ -25,9 +25,6 @@ path_username = fullfile(filesep,s{1:sel});
 
 % % build some paths
 path_spm12 = fullfile(path_username, 'spm12');
-%path_fieldtrip = fullfile(path_spm12, 'external', 'fieldtrip');
-%path_fileio = fullfile(path_fieldtrip, 'fileio');
-%path_freesurfer = fullfile(path_fieldtrip, 'external/freesurfer');
 path_config = fullfile(path_spm12, 'config');
 path_matlabbatch = fullfile(path_spm12, 'matlabbatch');
 path_input = fullfile(path_username, 'input');
@@ -36,9 +33,6 @@ path_workpackage = fullfile(path_username, s{(sel+1):end});
 
 % addpath, for matlab and use them 
 addpath(path_spm12);
-% addpath(path_fieldtrip);
-% addpath(path_fileio);
-% addpath(path_freesurfer);
 addpath(path_config);
 addpath(path_matlabbatch);
 addpath(path_workpackage);
@@ -53,18 +47,12 @@ pathSubject_delete = fullfile(path_input, 'sub-SAXNES2s006');
 if exist(pathSubject_delete, 'dir')
   removeSubject(pathSubject_delete);
 end
-
-% convert tsv files to txt files located at path_input 
-%convert_tsv_to_txt(path_input);
 	
 % create output architecture directory based on input directory
 createDataStructure(path_input, path_output);
 
 % apply a list of transformations to nifti files (anat + func)
 patientsDatabase(path_input, path_output, sub_list, task_list, run_list);
-
-% clean useless files
-cleanDatabase(path_input);
 
 time_end = toc(time_start);
 disp(time_end);
