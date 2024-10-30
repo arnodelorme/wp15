d1d88d64e2b314c1ce273d90ec4fcaa9b24c92c9
Marque-CerCo <adrien.romain.marque@gmail.com>
2024-10-30 12:46:19 +0100

    Update spm_preprocess.m

--- usecase-2.5/source/spm_preprocess.m
+++ usecase-2.5/source/spm_preprocess.m
@@ -1,71 +1,40 @@
-function [structPreProcess, funcfiles] = spm_preprocess(path_input, task_list, run_list)
+function spm_preprocess(path_input, path_output)
+	
+	path_source = '';
+	path_anat = fullfile(path_input, 'anat');
+	list_files = dir(path_anat);
+	szFiles = size(list_files);
 
-if nargin<2 || isempty(task_list) || (ischar(task_list) && isequal(task_list, 'all'))
-  task_list = {}; % don't do a selection on the tasks
-end
+	for f = 3:szFiles(1)
+		path_file = fullfile(path_anat, list_files(f).name);
+		check_nifti = endsWith(list_files(f).name, '.nii');
+		
+		if check_nifti == true
+			path_source = path_file;
+		end
+	end
 
-if nargin<3 || isempty(run_list) || (ischar(run_list) && isequal(run_list, 'all'))
-  run_list = {}; % don't do a selection on the runs
-end
+	list_fmris = dir(path_input);
+	szFMRIs = size(list_fmris);	
 
-path_source = '';
-path_anat = fullfile(path_input, 'anat');
-list_files = dir(fullfile(path_anat, 'sub*nii.gz'));
-if ~isempty(list_files)
-  for f = 1:numel(list_files)
-    fname = fullfile(list_files(f).folder, list_files(f).name);
-    fprintf('unzipping %s', fname)
-    gunzip(fname);
-    delete(fname);
-  end
-end
-list_files = dir(fullfile(path_anat, 'sub*nii'));
-szFiles = size(list_files);
+	for m = 3:szFMRIs(1)
+		path_fmri = fullfile(path_input, list_fmris(m).name);
+		check_fmri = isfolder(path_fmri);			
 
-for f = 1:szFiles(1)
-  path_file   = fullfile(path_anat, list_files(f).name);
-  path_source = path_file;
-end
-
-path_func = fullfile(path_input, 'func'); % according to BIDS, functional MRIs are always stored in func folders
-list_fmris = dir(fullfile(path_func, 'sub*nii.gz')); 
-if ~isempty(list_fmris)
-  for f = 1:numel(list_fmris)
-    fname = fullfile(list_fmris(f).folder, list_fmris(f).name);
-    fprintf('unzipping %s', fname)
-    gunzip(fname);
-    delete(fname);
-  end
-end
-list_fmris = dir(fullfile(path_func, 'sub*nii'));
-
-funcfiles  = {list_fmris.name}';
-sel        = false(numel(funcfiles), 1);
-if ~isempty(task_list)
-  for k = 1:numel(task_list)
-    sel = sel | contains(funcfiles, task_list{k});
-  end
-  funcfiles = funcfiles(sel);
-end
-sel        = false(numel(funcfiles), 1);
-if ~isempty(run_list)
-  for k = 1:numel(run_list)
-    sel = sel | contains(funcfiles, run_list{k});
-  end
-end
-funcfiles = funcfiles(sel);
-
-structPreProcess = struct;
-
-disp('Step 2 -- Realign all volumes to first functional volume');
-%realign(path_func, funcfiles);
-disp('Step 2 - Done!');
-  
-disp('Step 3 -- Coregister structural image to first dynamic image');
-%coreg(path_source, path_func, funcfiles);
-disp('Step 3 - Done!');
-
-disp('Step 4 -- Gaussian kernel smoothing of realigned data');
-%smooth(path_func, funcfiles); % FIXME CHECK WITH CerCo PEOPLE, the input to smooth() was path_anat at first, is that correct????
-disp('Step 4 is done !');
+		if check_fmri == true	
+			disp('Step 2 -- Realign all volumes to first functional volume');
+			realign(path_fmri, path_output);
+			disp('Step 2 - Done!');
+        end
+    end
+												
+	path_func = fullfile(path_input, 'func');
+		
+	disp('Step 3 -- Coregister structural image to first dynamic image');
+	coreg(path_source, path_func, path_output);
+	disp('Step 3 - Done!'); 
+									
+	disp('Step 4 -- Gaussian kernel smoothing of realigned data');
+	smooth(path_anat);
+	disp('Step 4 is done !');
 end
