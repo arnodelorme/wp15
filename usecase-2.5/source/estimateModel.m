c1cf8f9e179f33e1c3cfe71681dd7bce375ace46
Marque-CerCo <adrien.romain.marque@gmail.com>
2024-10-30 10:28:27 +0100

    Update estimateModel.m

--- usecase-2.5/source/estimateModel.m
+++ usecase-2.5/source/estimateModel.m
@@ -1,27 +1,21 @@
 % estimate model function, the result is stored on save directory
-function estimateModel(path_output, contrast)
+function estimateModel(path_output)
+	 	
+	path_spmmat = fullfile(path_output, 'SPM.mat');
 
-    % setup batch job structure
-    model_estimation = struct;
-	 
-    list_runs = dir(path_output);
-    szRuns= size(list_runs);	
-	
-	for r = 3:szRuns(1)
-		path_run = fullfile(path_output, list_runs(r).name);
-		path_spmmat = fullfile(path_run, 'SPM.mat');
-
-     	% spmmat
-     	model_estimation.matlabbatch{1}.spm.stats.fmri_est.spmmat = cellstr(path_spmmat); %path_spmmat
+    % spmmat
+    matlabbatch{1}.spm.stats.fmri_est.spmmat = cellstr(path_spmmat); %path_spmmat
    
-     	% write_residuals
-    	model_estimation.matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
+    % write_residuals
+    matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
 
-    	% method
-		model_estimation.matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
+    % method
+	matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
         
-		% run batch job
-		spm_jobman('run',model_estimation.matlabbatch);
-		clear matlabbatch;
-	end
+	% run batch job
+	spm_jobman('run', matlabbatch);
+	clear matlabbatch;
+
+    path_estimate = fullfile(path_output, 'estimate_model.mat');
+    copyfile(path_spmmat, path_estimate);
 end
