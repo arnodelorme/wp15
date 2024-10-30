2d160cbed3080326c82a46ff19528bd08912448a
Marque-CerCo <adrien.romain.marque@gmail.com>
2024-10-30 10:48:36 +0100

    Update onePatient.m

--- usecase-2.5/source/onePatient.m
+++ usecase-2.5/source/onePatient.m
@@ -2,8 +2,8 @@
 function onePatient(path_username, path_subject, path_output)
 
 	% apply list of transformations to fMRIs
-  spm_preprocess(path_subject, task_list, run_list);
-  disp('preProcess is done !');
+    	[s, funcfiles] = spm_preprocess(path_subject, path_output);
+    	disp('preProcess is done !');
 		
 	path_input = fullfile(path_username, 'input');
 	path_output = fullfile(path_username, 'output');
@@ -24,6 +24,7 @@ function onePatient(path_username, path_subject, path_output)
 
 			if check_sub == true && check_nifti == true
 				path_tsv = fullfile(path_func, list_files(f+1).name);
+				path_txt = fullfile(path_func, list_files(f+2).name);
 				run_event = extractEvents(list_files(f+1).name);
 				events = dataEvents(path_txt, 'event');
 				onset =  dataEvents(path_txt, 'onset');
@@ -43,20 +44,24 @@ function onePatient(path_username, path_subject, path_output)
 				contrasts(path_run);
 				disp('contrasts is done !');
 			end
-    end
-  end
+        	end
 
-  path_anat = fullfile(path_subject, 'anat');
-  list_files = dir(path_anat);
-  szFiles = size(list_files);
+        	path_anat = fullfile(path_subject, 'anat');
+        	list_files = dir(path_anat);
+        	szFiles = size(list_files);
 
-  for f = 3:szFiles(1)
-      check_sub = startsWith(list_files(f).name, 'sub');
-      if check_sub == false
-          path_file = fullfile(path_anat, list_files(f).name);
-          delete(path_file);
-      end
-  end 
+        	for f = 3:szFiles(1)
+            		check_sub = startsWith(list_files(f).name, 'sub');
+
+            		if check_sub == false
+                		path_file = fullfile(path_anat, list_files(f).name);
+                		delete(path_file);
+            		end
+        	end 
+	end
+
+	path_ref = fullfile(path_output, 'sub-SAXNES2s001');
+	list_runs = dir(path_ref);
 	
 	secondLevel(path_output, list_runs, "contrast");
 	disp('secondLevel is done !');
