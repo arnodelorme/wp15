e6d84a46ee2b71e51cbb12a5334a0141e8bcfbf7
Marque-CerCo <adrien.romain.marque@gmail.com>
2024-10-30 09:56:26 +0100

    Update coreg.m

--- usecase-2.5/source/coreg.m
+++ usecase-2.5/source/coreg.m
@@ -1,48 +1,73 @@
-function coreg(path_source, path_func, funcfiles)
+function coreg(path_source, path_func, path_output)
+    			
+	listFunc_files = dir(path_func);
+	szFunc = size(listFunc_files);
+	
+	for f = 3:szFunc(1)
+		check_mean = startsWith(listFunc_files(f).name, "mean");
+				
+		if check_mean == true 
+			path_ref = fullfile(path_func, listFunc_files(f).name);
+					
+			matlabbatch = {};
+								
+			% Ref
+			matlabbatch{1}.spm.spatial.coreg.estimate.ref = cellstr(path_ref);
+        
+			% Source
+			matlabbatch{1}.spm.spatial.coreg.estimate.source = cellstr(path_source);
+        
+			% Eoptions
+			matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
+			matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+			matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+			matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
+        
+			% Run
+			spm_jobman('run',matlabbatch);    
+			clear matlabbatch;
+		end 
+	end
 
-if nargin<3
-  funcfiles = {};
-end
+	list_files = dir(path_func);
+	szFiles = size(list_files);
 
-coreg_estimate = struct;
+	for f = 3:szFiles(1)
+		check_rp = startsWith(list_files(f).name, 'rp_');
+		check_mat = endsWith(list_files(f).name, '.mat');
+		check_mean = startsWith(list_files(f).name, 'mean');
+		check_rsub = startsWith(list_files(f).name, 'rsub');
 
-list_files = dir(fullfile(path_func, 'mean*nii'));
-if isempty(funcfiles)
-  funcfiles  = {list_files.name}';
-else
-  % the funcfiles are to be prepended with 'mean'
-  for f = 1:numel(funcfiles)
-    funcfiles{f} = sprintf('mean%s', funcfiles{f});
-  end
-end
+		if check_mat == true
+			path_mat = fullfile(path_func, list_files(f).name);
+			event = extractEvents(list_files(f).name);
+			path_event = fullfile(path_output, event);
+			copyfile(path_mat, path_event);
+			delete(path_mat);
+		end 
+		
+		if check_rp == true 
+			path_rp = fullfile(path_func, list_files(f).name);
+			event = extractEvents(list_files(f).name);
+			path_event = fullfile(path_output, event);
+			copyfile(path_rp, path_event);
+			delete(path_rp);
+		end 
 
-for f = 1:numel(funcfiles)
-  check_mean = startsWith(funcfiles{f}, 'mean');
+		if check_mean == true
+			path_mean = fullfile(path_func, list_files(f).name);
+			event = extractEvents(list_files(f).name);
+			path_event = fullfile(path_output, event);
+			copyfile(path_mean, path_event);
+			delete(path_mean);
+		end
 
-  if check_mean == true
-    path_ref = fullfile(path_func, funcfiles{f});
-
-    matlabbatch = {};
-
-    disp(path_source);
-    disp(path_ref);
-
-    % Ref
-    coreg_estimate.matlabbatch{1}.spm.spatial.coreg.estimate.ref = cellstr(path_ref);
-
-    % Source
-    coreg_estimate.matlabbatch{1}.spm.spatial.coreg.estimate.source = cellstr(path_source);
-
-    % Eoptions
-    coreg_estimate.matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
-    coreg_estimate.matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
-    coreg_estimate.matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
-    coreg_estimate.matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
-
-    % Run
-    spm_jobman('run',coreg_estimate.matlabbatch);
-
-    clear matlabbatch;
-  end
-end
+		if check_rsub == true
+			path_rfile = fullfile(path_func, list_files(f).name);
+			event = extractEvents(list_files(f).name);
+			path_event = fullfile(path_output, event);
+			copyfile(path_rfile, path_event);
+			delete(path_rfile);
+		end
+	end
 end
