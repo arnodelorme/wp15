06b4a552f4d6ef8b5098cffc7a9a11f0d3588281
Marque-CerCo <adrien.romain.marque@gmail.com>
2024-10-30 10:53:20 +0100

    Update smooth.m

--- usecase-2.5/source/smooth.m
+++ usecase-2.5/source/smooth.m
@@ -1,38 +1,34 @@
 function smooth(path_func, funcfiles)
 
-if nargin<3
-  funcfiles = {};
-end
+  if nargin<3
+    funcfiles = {};
+  end
 
-list_files = dir(fullfile(path_func, 'r*nii'));
-if isempty(funcfiles)
-  funcfiles  = {list_files.name}';
-else
-  % the funcfiles are to be prepended with 'mean'
+  list_files = dir(fullfile(path_func, 'r*nii'));
+  if isempty(funcfiles)
+    funcfiles  = {list_files.name}';
+  else
+    % the funcfiles are to be prepended with 'mean'
+    for f = 1:numel(funcfiles)
+      funcfiles{f} = sprintf('r%s', funcfiles{f});
+    end
+  end
+
   for f = 1:numel(funcfiles)
-    funcfiles{f} = sprintf('r%s', funcfiles{f});
+    check_rFile = startsWith(funcfiles{f}, 'rsub');
+
+    if check_rFile == true
+      path_file = fullfile(path_func, funcfiles{f});
+
+      % Data
+      matlabbatch{1}.spm.spatial.smooth.data = cellstr(path_file);
+      matlabbatch{1}.spm.spatial.smooth.fwhm = [8 8 8];
+      matlabbatch{1}.spm.spatial.smooth.dtype = 0;
+      matlabbatch{1}.spm.spatial.smooth.prefix = 's';
+
+      % Run
+      spm_jobman('run', matlabbatch);
+      clear matlabbatch;
+    end
   end
-end
-
-for f = 1:numel(funcfiles)
-  check_rFile = startsWith(funcfiles{f}, 'rsub');
-
-  if check_rFile == true
-    path_file = fullfile(path_func, funcfiles{f});
-
-    smooth = struct;
-
-    % Data
-    smooth.matlabbatch{1}.spm.spatial.smooth.data = cellstr(path_file);
-    smooth.matlabbatch{1}.spm.spatial.smooth.fwhm = [8 8 8];
-    smooth.matlabbatch{1}.spm.spatial.smooth.dtype = 0;
-    smooth.matlabbatch{1}.spm.spatial.smooth.prefix = 's';
-
-    % Run
-    spm_jobman('run',smooth.matlabbatch);
-
-    clear matlabbatch;
-
-  end
-end
 end
