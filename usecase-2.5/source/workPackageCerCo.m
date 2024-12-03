7afeaeae1cfcddaa3dce51a86a34f613592b21a1
Robert Oostenveld <r.oostenveld@gmail.com>
2024-12-03 14:14:53 +0100

    add the participant/group level to the function call, set the MATLAB path externally

--- usecase-2.5/source/workPackageCerCo.m
+++ usecase-2.5/source/workPackageCerCo.m
@@ -1,41 +1,14 @@
-function workPackageCerCo(sub_list, task_list, run_list)
+function workPackageCerCo(path_input, path_output, level, sub_list, task_list)
 
-if nargin<1 || isempty(sub_list)
+if nargin<4 || isempty(sub_list)
   sub_list = 'all';
 end
 
-if nargin<2 || isempty(task_list)
+if nargin<5 || isempty(task_list)
   task_list = 'all';
 end
 
-if nargin<3 || isempty(run_list)
-  run_list = 'all';
-end
-
 time_start = tic;
-
-m = mfilename('fullpath');
-[p,f,e] = fileparts(m);
-s = split(p, filesep);
-sel = find(strcmp(s,'wp15'))-1;
-path_username = fullfile(filesep,s{1:sel});
-
-% this assumes that the wp15 repo is at the same level as spm12 and the
-% input/output folders, and that the spm12 distro is NOT in spm/spm12
-
-% % build some paths
-path_spm12 = fullfile(path_username, 'spm12');
-path_config = fullfile(path_spm12, 'config');
-path_matlabbatch = fullfile(path_spm12, 'matlabbatch');
-path_input = fullfile(path_username, 'input');
-path_output = fullfile(path_username, 'output');
-path_workpackage = fullfile(path_username, s{(sel+1):end});
-
-% addpath, for matlab and use them 
-addpath(path_spm12);
-addpath(path_config);
-addpath(path_matlabbatch);
-addpath(path_workpackage);
 
 % initialize spm12 via matlab
 spm('defaults','fmri');
@@ -52,12 +25,12 @@ end
 deleteVOE(path_input);
 	
 % create output architecture directory based on input directory
-if ~exist(path_output)
+if ~exist(path_output, 'dir')
   createDataStructure(path_input, path_output);
 end
 
 % apply a list of transformations to nifti files (anat + func)
-patientsDatabase(path_input, path_output, sub_list, task_list);
+patientsDatabase(path_input, path_output, level, sub_list, task_list);
 
 time_end = toc(time_start);
 disp(time_end);
